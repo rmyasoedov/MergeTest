1dd4202438c43348669e3851c080b858c0853473
Roman Muasoedov <375298131149@yandex.by>
2022-03-27 14:08:53 +0300

    Добавлен калькулятор разности

--- app/src/main/java/com/example/mergetest/MyFun.kt
+++ app/src/main/java/com/example/mergetest/MyFun.kt
@@ -2,6 +2,7 @@ package com.example.mergetest
 
 class MyFun {
     companion object{
-
+        fun Diff(a1: Double, a2: Double): Double =
+            a1 - a2
     }
 }
--- app/src/main/java/com/example/mergetest/frame2/Frame2Fragment.kt
+++ app/src/main/java/com/example/mergetest/frame2/Frame2Fragment.kt
@@ -5,15 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.example.mergetest.MyFun
 import com.example.mergetest.R
 
+
 class Frame2Fragment : Fragment() {
+
+    lateinit var a1: EditText
+    lateinit var a2: EditText
+    lateinit var buttonDiff: Button
+    lateinit var resultField: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frame2, container, false)
+        val root = inflater.inflate(R.layout.fragment_frame2, container, false)
+
+        a1 = root.findViewById(R.id.a1)
+        a2 = root.findViewById(R.id.a2)
+        buttonDiff = root.findViewById(R.id.buttonDiff)
+        resultField = root.findViewById(R.id.resultField)
+
+        buttonDiff.setOnClickListener {
+            val sum = MyFun.Diff(a1.text.toString().toDouble(),a2.text.toString().toDouble())
+            resultField.setText(sum.toString())
+        }
+
+
+        return root
     }
 }
