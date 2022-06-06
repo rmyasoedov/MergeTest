9227ec1cdf34b9cb0902cb276568795a4d048318
Roman Muasoedov <375298131149@yandex.by>
2022-06-07 00:29:30 +0300

    test commit branch

--- app/src/main/java/com/example/mergetest/MyConst.kt
+++ app/src/main/java/com/example/mergetest/MyConst.kt
@@ -2,7 +2,8 @@ package com.example.mergetest
 
 class MyConst {
     companion object{
-        const val DEFAULT_VALUE = 30
+        const val DEFAULT_VALUE = 60
         const val LABEL_BUTTON = "Calculate"
+        const val SECONT_CONST = 90
     }
 }
